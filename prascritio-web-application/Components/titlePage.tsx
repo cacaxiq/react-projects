1ebe16ce894122768ac5c06e683e721b108ac70d
Carlos Henrique Rocha Gabriel <chrochagabriel@icloud.com>
2024-06-14 11:56:05 +0100

    Files removed from .gitignore

--- prascritio-web-application/Components/titlePage.tsx
+++
@@ -1,9 +0,0 @@
-export default function TitlePage(props: { title: String }) {
-    return (
-        <nav className="navbar bg-secondary border rounded-end">
-            <div className="container-fluid">
-                <span className="navbar-brand mb-0 h1 text-white">{props.title}</span>
-            </div>
-        </nav>
-    )
-}
